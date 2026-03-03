8771288750e20b31b9be07ae80f892b7dc56a511
Rakesh Yadav <podamekalarakeshyadavb@gmail.com>
2026-03-03 11:08:25 +0530

    added features like : unique email,unique username,relationship mapping

--- models.py
+++ models.py
@@ -4,15 +4,20 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
-    email = db.Column(db.String(100))
-    phone = db.Column(db.String(20))
-    password = db.Column(db.String(50))
+
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    foods = db.relationship('Food', backref='user', lazy=True)
 
 
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    expiry = db.Column(db.Date)
+
+    name = db.Column(db.String(100), nullable=False)
+    expiry = db.Column(db.Date, nullable=False)
     last_alert_time = db.Column(db.DateTime, nullable=True)
-    user_id = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
