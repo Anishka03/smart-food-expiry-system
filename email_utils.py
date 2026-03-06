42f1edd1c6dcce4f1d2f60a639f4e57b8e5c48ed
Rakesh Yadav <podamekalarakeshyadavb@gmail.com>
2026-03-07 00:12:04 +0530

    Add SMTP email notification service for food expiry alerts

--- email_utils.py
+++ email_utils.py
@@ -1,6 +1,6 @@
 import smtplib
-from email.mime.text import MIMEText
 import os
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,17 +8,25 @@ load_dotenv()
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 
+
 def send_email(to, msg):
     try:
         message = MIMEText(msg)
-        message['Subject'] = "Food Expiry Alert"
-        message['From'] = SENDER_EMAIL
-        message['To'] = to
+        message["Subject"] = "Food Expiry Alert"
+        message["From"] = SENDER_EMAIL
+        message["To"] = to
 
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
+
         server.login(SENDER_EMAIL, SENDER_PASSWORD)
-        server.sendmail(SENDER_EMAIL, to, message.as_string())
+
+        server.sendmail(
+            SENDER_EMAIL,
+            to,
+            message.as_string()
+        )
+
         server.quit()
 
         print("Email sent")
