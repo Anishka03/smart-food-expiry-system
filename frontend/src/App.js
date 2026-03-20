5de85d21c5ada0a68ef4faae53e1c5eddcd170c4
Rakesh Yadav <podamekalarakeshyadavb@gmail.com>
2026-03-21 00:17:23 +0530

    Update with food list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login.tsx";
 import { Dashboard } from "./components/Dashboard.tsx";
+import { FoodList } from "./components/FoodList.tsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/foods" element={<FoodList />} />
     </Routes>
   );
 }
